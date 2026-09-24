23b95cb336c609af91f674ff6d3613448cdcbd82
wwisp-154 <3390412381@qq.com>
2026-09-24 20:00:24 +0800

    我写完了01.py的注释练习

--- scripts/01.py
+++ scripts/01.py
@@ -1,3 +1,10 @@
-print('hi')
+# 学习 Python 语法中的注释
+print("Hello, World!")
 
+# 这是一个注释，用于解释代码的功能
+# 注释不会被 Python 解释器执行，只是用来说明代码的作用
+# 注释可以放在代码的上方、下方或旁边，帮助其他人理解代码的逻辑
 
+# Python 中的变量
+# 变量是用于存储数据的命名空间。在 Python 中，变量不需要声明类型
+print("这段代码运行在 Jupyter Notebook 中的 Python 3 环境")
